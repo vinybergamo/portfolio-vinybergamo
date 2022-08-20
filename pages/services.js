65ffdb51feea1fa8dc1f8e261f68c678af29f0e2
TantoFaz <vinybergamo@gmail.com>
2022-08-20 14:29:49 -0300

    Services atualizado

--- pages/services.js
+++ pages/services.js
@@ -5,9 +5,20 @@ import ComingSon from "../components/coming-son";
 export default function Home() {
   return (
     <>
-      <Header />
+      <Header
+        logo="Vinny"
+        about="About me"
+        services="Services"
+        contact="Contact me"
+        lang="PT-BR"
+        logoLink="#"
+        aboutLink="#about"
+        contactLink="#contact"
+        langLink="/pt-br"
+        langImg="./assets/images/brazil-flag.png"
+      />
       <ComingSon />
-      <Footer />
+      <Footer link="/" rights="All rights reserved" />
     </>
   );
 }
